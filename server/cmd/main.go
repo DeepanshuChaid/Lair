200790c9a810e3f348af948c1b0f19b8c90a1302
Deepanshu Chaid <bantar117@gmail.com>
2026-03-15 20:48:05 +0000

    BASIC WEB SOCKET

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,10 +3,61 @@ package main
 import (
 	"log"
 	"os"
+  "net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+  "github.com/gorilla/websocket"
 )
+
+// THIS CONVERTS THE HTTP CONNECTION TO A WEBSOCKET CONNECTION
+// Client: HTTP request
+// Server: 101 Switching Protocols
+// Connection becomes WebSocket
+var upgrader = websocket.Upgrader{
+  CheckOrigin: func(r *http.Request) bool {
+    return true
+  },
+}
+
+func handleWS (c *gin.Context) {
+  // Upgrade the connection to a websocket connection
+  conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+  
+  if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "error": err.Error(),
+    })
+    return 
+  }
+
+  // Read messages from the client for loop does not hurt because it is a websocket connection basically a persistent connection
+  // browser sends message
+  // ↓
+  // server receives
+  // ↓
+  // server prints it
+  for {
+    var msg string
+
+    err := conn.ReadJSON(&msg)
+    if err != nil {
+      log.Println("Error reading message: ", err)
+      break
+    }
+
+    log.Println("Message received: ", msg)
+  }
+
+  
+  log.Println("conn", conn)
+  log.Println("CLIENT CONNECTED")
+
+  // Send a message to the client
+  conn.WriteJSON("message revieved yipeee!")
+}
+
 
 func main(){
   err := godotenv.Load()
@@ -14,15 +65,17 @@ func main(){
     log.Fatal("Error loading .env file")
   }
   
-  r := gin.Default()
+  router := gin.Default()
 
-  r.GET("/", func(c *gin.Context){
+  router.GET("/", func(c *gin.Context){
     c.JSON(200, gin.H{
       "message": "Hello World",
     })
   })
 
+  router.GET("/ws", handleWS)
+
   PORT := os.Getenv("PORT")
 
-  r.Run(":" + PORT)
+  router.Run(":" + PORT)
 }
