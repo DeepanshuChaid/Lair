e24389e6a26601e4c3bc2c5ada841ad2d4eb4750
Deepanshu Chaid <bantar117@gmail.com>
2026-03-16 17:10:19 +0000

    LEARNT HOW BASIC HUB WORKS

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,27 +2,39 @@ package main
 
 import "fmt"
 
+// chan are used for sending and revieveing data in goruntines
 type hub struct {
   broadcast chan []byte
 }
 
+// simply returns a hub
 func newHub() *hub {
   return &hub{
     broadcast: make(chan []byte),
   }
 }
 
+// this is a method of hub a infinite loop where it waits for the ALL THE sent messages and just prints it
 func (h *hub) run () {
   for {
-    message := <-h.broadcast
-    fmt.Println("HUB revieved", string(message))
+    
+    for message := range h.broadcast {
+      fmt.Println("HUb recevied", string(message))
+    }
+    
   }
 }
+
 
 func main () {
   hub := newHub()
 
   go hub.run()
 
+  // SENDING DATA
   hub.broadcast <- []byte("Hello from hub")
+
+  hub.broadcast <- []byte("Hello from hub again")
+
+  hub.broadcast <- []byte("HATE HATE HATE IF THE WORD HATE WAS ENGRAVED ON EACH CELL ON MY BODY IT WOULD BE EQUAL TO THE ONE BILLIONTH OF THE HATE I FEEL TOWARDS YOU AT THIS MICRO INSTANT HATE HAHAH HATEEE!")
 }
