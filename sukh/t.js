3738ad4b0620eca1dd08534072b61125a36a6acb
Deepanshu Chaid <bantar117@gmail.com>
2026-03-16 19:18:15 +0000

    TEST WITH TWO SERVERS

--- sukh/t.js
+++ sukh/t.js
@@ -1,13 +1,14 @@
 const WebSocket = require("ws")
 
-const ws = new WebSocket("wss://c3928af3-6033-44b9-8ba2-5c6f13fb9363-00-1nkw60jgj59p1.sisko.replit.dev/ws");
+// change this if your URL is different
+const ws = new WebSocket("wss://c3928af3-6033-44b9-8ba2-5c6f13fb9363-00-1nkw60jgj59p1.sisko.replit.dev/ws")
 
 ws.on("open", () => {
   console.log("Connected to server")
 
   // send a message every 3 seconds
   setInterval(() => {
-    const msg = "Hello from Node No-2" + Date.now()
+    const msg = "Hello from Node " + Date.now()
     console.log("Sending:", msg)
 
     ws.send(msg)
